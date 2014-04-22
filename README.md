357f951498e4221dca27f5b9383a2bc023cad214
Roberto Martin <robert.90@gmail.com>
2014-04-22 19:38:34 +0200

    First commit

--- README.md
+++ README.md
@@ -2,3 +2,33 @@ nodetsdb
 ========
 
 Simple Node module to get datapoints from an OpenTSDB database
+
+## Authors
+
+* Roberto Martin
+
+## Installation
+
+  npm install nodetsdb --save
+
+## Usage
+
+
+```javascript
+var nodetsdb = new Nodetsdb({host:'opentsdbserver.com', port:4242});
+
+var queryconf = {start:'2013/04/04-12:00:00',end:'2013/04/18-15:46:17', metric:'city.weather.temp', aggregator:'avg', tags:{id:44640, quality:0}};
+  
+nodetsdb.getDataPoints(queryconf, function(datapoints){
+    if(datapoints){
+        console.log(datapoints);
+    }else{
+        console.log('No datapoints');
+    }
+});
+
+```
+
+## Release History
+
+* 0.1.0 Initial release
---
+++ module.js
@@ -0,0 +1,48 @@
+var http = require('http');
+
+function Nodetsdb(configuration){
+    if(!configuration.host || !configuration.port){
+        throw 'Please provide a host and a port';
+    }
+    
+    this.host = configuration.host;
+    this.port = configuration.port;
+    
+    this.getDataPoints = function(query, callback){
+        if(!query.start || !query.end || !query.metric || !query.aggregator){
+            throw 'Query parameters missing, min start, end, metric, aggregator';
+        }
+        var queryURL = "http://"+this.host+":"+this.port+"/api/query?start="+query.start+"&end="+query.end+"&m="+query.aggregator+":"+query.metric;
+        if(query.tags){
+        queryURL += '{';
+            var ntags = Object.keys(query.tags).length;
+            var j = 1;
+            for(i in query.tags){
+                if(j != ntags){
+                    queryURL += i+'='+query.tags[i]+",";
+                }else{
+                    queryURL += i+'='+query.tags[i];
+                }
+                j++;
+            }
+        queryURL += '}';            
+        }else{
+            queryURL +='{}';
+        }
+        
+      //Query correctly created
+      http.get(queryURL, function(ress) {
+          var responseParts="";
+          ress.on('data', function (chunk) {
+                responseParts+=chunk;
+          });
+          ress.on('end', function () {
+            callback(responseParts);
+          });
+       
+       }).on('error', function(e) {
+           callback();
+       });
+     
+    }
+}
